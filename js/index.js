33d8bc4f363fc752d55093db17422bb7ee3e8c28
Wender Machado <devwendermachado@gmail.com>
2025-04-27 20:37:07 -0300

    adiciona feature de accordion no faq style: adiciona animações simples de entrada

--- js/index.js
+++ js/index.js
@@ -1,19 +1,41 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
-tabContent[0].classList.add('ativo')
-
-if(tabMenu.length && tabContent.length){
-  function activeTab(index){
-    tabContent.forEach((section)=>{
-      section.classList.remove('ativo')
+function initTabnav(){
+  const tabMenu = document.querySelectorAll('.js-tabmenu li')
+  const tabContent = document.querySelectorAll('.js-tabcontent section')
+  tabContent[0].classList.add('ativo')
+  
+  if(tabMenu.length && tabContent.length){
+    function activeTab(index){
+      tabContent.forEach((section)=>{
+        section.classList.remove('ativo')
+      })
+      tabContent[index].classList.add('ativo')
+    }
+    
+    tabMenu.forEach((itemMenu, index)=>{
+      itemMenu.addEventListener('click', () =>{
+        activeTab(index)
+      })
     })
-    tabContent[index].classList.add('ativo')
   }
   
-  tabMenu.forEach((itemMenu, index)=>{
-    itemMenu.addEventListener('click', () =>{
-      activeTab(index)
-    })
+}
+
+initTabnav()
+
+function initAccordion(){
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  accordionList[0].classList.add('ativo')
+  accordionList[0].nextElementSibling.classList.add('ativo')
+  
+  function activeAccordion(){
+    this.classList.toggle('ativo')
+   this.nextElementSibling.classList.toggle('ativo')
+  }
+  
+  accordionList.forEach((item) =>{
+    item.addEventListener('click', activeAccordion)
   })
 }
+initAccordion()
+
 
