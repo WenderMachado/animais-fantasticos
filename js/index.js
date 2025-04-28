d3d4fc2da809a046fd45dfd81bab23b1e00dd8fc
Wender Machado <devwendermachado@gmail.com>
2025-04-27 21:24:31 -0300

    adiciona scroll relativo ao tamanho da tela

--- js/index.js
+++ js/index.js
@@ -57,3 +57,15 @@ function initScroll(){
   })
 }
 initScroll()
+
+const sections = document.querySelectorAll('.js-scroll')
+const windowMetade = window.innerHeight * 0.6
+function animaScroll(){
+ sections.forEach((section)=>{
+  const sectionTop = section.getBoundingClientRect().top - windowMetade
+  if(sectionTop < 0){
+    section.classList.add('ativo')
+  }
+ })
+}
+window.addEventListener('scroll', animaScroll)
