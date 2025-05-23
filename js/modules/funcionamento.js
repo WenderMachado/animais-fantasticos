c984ad6e903243eaed07f0dff6eb4c05af12ed6b
Wender Machado <devwendermachado@gmail.com>
2025-05-23 16:31:04 -0300

    refatora elementos de falas

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,21 +1,16 @@
-export default function initFuncionamento(){
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector('[data-semana]');
+  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
   
-}
-
-const funcionamento = document.querySelector('[data-semana]')
-const diasSemana = funcionamento.dataset.semana.split(',').map(Number)
-const horariosSemana = funcionamento.dataset.horario.split(',').map(Number)
-
-console.log(horariosSemana)
-
-const dataAgora = new Date()
-const diaAgora = dataAgora.getDay()
-const horarioAgora = dataAgora.getHours()
-
-const semanaAberta = diasSemana.indexOf(diaAgora) !== -1
-
-const horarioAberto = (horarioAgora >= horariosSemana[0] &&  horarioAgora < horariosSemana[1])
-
-if(semanaAberta && horarioAberto){
-  funcionamento.classList.add('aberto')
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours();
+  
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+  const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
+  
+  if(semanaAberto && horarioAberto) {
+    funcionamento.classList.add('aberto');
+  }
 }
